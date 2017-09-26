765594a4d4ddc6c6ee81fc534b6491427f75d738
managercompany <31020711+managercompany@users.noreply.github.com>
2017-09-26 17:32:18 -0500

    THING

--- mah/code/sendover.php
+++ mah/code/sendover.php
@@ -1,3 +1,5 @@
+<?php
+header("Access-Control-Allow-Origin: *")
 if(!empty($_POST['data'])){
 $data = $_POST['data'];
 $fname = mktime() . ".txt";//generates random name
@@ -6,3 +8,4 @@ $file = fopen("upload/" .$fname, 'w');//creates new file
 fwrite($file, $data);
 fclose($file);
 }
+?>
